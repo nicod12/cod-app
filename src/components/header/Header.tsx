5c824373124a8d65501e60122afa7f03dbc56a82
nicod12 <dumenicolas45@gmail.com>
2023-06-19 21:34:04 -0300

    v2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
-
+import { useEffect, useState } from "react"
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 interface Items {
     title:string
@@ -22,23 +23,53 @@ const menuItems:Items[] = [
 
 ]
 
+const MenuMob = () => {
+    return(
+
+        <div className="my-0 mx-auto flex items-center gap-40 md:hidden lg:hidden xl:hidden 2xl:hidden 3xl:hidden">
+            <h2>logo</h2>
+            <FaBars/>
+         </div>
+    )
+}
+
+
+const MenuNorm = () => {
+    return(
+        <div className="sm:hidden flex gap-[400px] my-0 mx-auto justify-between">
+         <h2>logo</h2>
+         <div className="flex gap-8 items-center">
+            {
+                menuItems.map(item => {
+                    return(
+                        <ul 
+                            key={item.key}
+                            
+                        >
+                        <li><Link to={`${item.key}`}>{item.title}</Link></li>
+                        </ul>
+                    )
+                })
+            }
+        </div>
+        </div>
+    )
+}
+
 const Header = () => {
    
+ 
     const [showMenu, setShowMenu] = useState("sm:hidden")
+
+
 
   return (
     <section>
         <div className="h-[800px] w-full bg-red-400">
             {/* <img src="" alt="" /> */}
-            <nav className="relative top-6 w-[50%] my-0 mx-auto flex items-center justify-between py-1 px-[20px] bg-transparent backdrop-blur-md">
-                <h2>logo</h2>
-                <div className="flex items-center">
-                    <ul className="flex gap-4">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Games</li>
-                    </ul>
-                </div>
+            <nav className="relative top-6 w-[50%] my-0 mx-auto flex items-center justify-between py-1 px-[20px] bg-transparent backdrop-blur-md">      
+                <MenuMob />
+                <MenuNorm />
             </nav>
         </div>
     </section>
